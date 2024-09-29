9fe2594ea83d0711d318d2e5bfee995848f48460
Pablo Bustos <pablobustos609@gmail.com>
2024-09-29 20:21:59 -0300

    create /send route for messages

---
+++ src/controllers/messages.ts
@@ -0,0 +1,61 @@
+import { Response, Request, } from "express";
+import prisma from "../prisma";
+
+export const sendMessage = async (req: Request, res: Response) => {
+  try {
+    const { message } = req.body
+    const { id: receiverID } = req.params
+    const senderID = req.user.id
+
+    // Primero busca si ya hay algun chat creado para estos dos usuarios
+    let conversation = await prisma.conversation.findFirst({
+      where: {
+        participantIds: {
+          hasEvery: [senderID, receiverID]
+        }
+      }
+    })
+
+    // De no existir, la creamos
+    if (!conversation) {
+      conversation = await prisma.conversation.create({
+        data: {
+          participantIds: {
+            set: [senderID, receiverID]
+          }
+        }
+      })
+    }
+
+    // Creamos el mensaje
+    const newMessage = await prisma.message.create({
+      data: {
+        senderId: senderID,
+        body: message,
+        conversationId: conversation.id
+      }
+    })
+
+    if (newMessage) {
+      conversation = await prisma.conversation.update({
+        where: {
+          id: conversation.id
+        },
+        data: {
+          messages: {
+            connect: {
+              id: newMessage.id
+            }
+          }
+        }
+      })
+    }
+
+    // TODO SocketIO implementation
+    
+    res.status(201).json({ newMessage })
+  } catch(e) {
+    res.status(500).json({ message: "Internal server error", error: e })
+    return
+  }
+}
--- src/routes/messages.routes.ts
+++ src/routes/messages.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import protect from "../middlewares/protect";
+import { sendMessage } from "../controllers/messages";
 
 const router = Router()
 
 router.get("/conversation", (req, res) => {
   res.send("conversations")
 })
+
+router.post("/send/:id", protect, sendMessage)
 
 export default router
